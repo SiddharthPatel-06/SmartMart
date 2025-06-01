c89c5345cc771342fef533dab5180ba30c5c4f8b
Siddharth Patel <connectwithsiddh@gmail.com>
2025-06-01 14:43:05 +0530

    Setup project structure for client-server app, created folders, connected MongoDB, and added concurrent run script

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="flex justify-center items-center h-screen text-xl">
+      Welcome to SmartMart
+    </h1>
+  );
+};
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const { connectDB } = require("./config/db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to SmartMart API");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
